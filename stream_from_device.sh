30866e412de5deee30fa503c8b843180695a378b
Manuel Bogner <manuel.bogner@openresearch.com>
2023-08-20 17:52:16 +0200

    deprecation

--- stream_from_device.sh
+++ stream_from_device.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-#ffmpeg -i tcp://192.168.0.248:9000 -c:v copy -f h264 -bufsize 128k - | ffplay -
-ffmpeg -i tcp://192.168.0.248:9000 -c:v copy -f h264 -bufsize 128 -vsync 0 - | ffplay -
+ffmpeg -i tcp://192.168.0.248:9000 -c:v copy -f h264 -bufsize 128k - | ffplay -
