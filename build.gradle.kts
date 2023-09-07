3734017066e8e336b82a07704da9ac3155f87f2d
Manuel Bogner <manuel.bogner@openresearch.com>
2023-09-07 18:10:40 +0200

    infra: introduce ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    // https://github.com/google/ksp/releases/tag/1.9.10-1.0.13
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13"
 }
