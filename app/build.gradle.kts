d1a23e01595e7228cffddb3c71246cbf4c03aa1c
Manuel Bogner <manuel.bogner@openresearch.com>
2024-07-22 08:37:49 +0200

    next version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 12
-        versionName = "1.0.11"
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
