4b749e1549d7290240edebdb3c08a3a5499a6ee3
Manuel Bogner <m@mbo.dev>
2026-02-25 23:56:29 +0100

    fix double tap start camera problem

--- app/src/main/java/dev/mbo/androidcamera/ui/NavigationHost.kt
+++ app/src/main/java/dev/mbo/androidcamera/ui/NavigationHost.kt
@@ -1,6 +1,7 @@
 package dev.mbo.androidcamera.ui
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -20,7 +21,9 @@ fun NavigationHost() {
             StartScreen(
                 viewModel = viewModel,
                 onStartCamera = {
-                    navController.navigate(Camera(viewModel.selectedLensFacing))
+                    if (navController.currentBackStackEntry?.lifecycle?.currentState == Lifecycle.State.RESUMED) {
+                        navController.navigate(Camera(viewModel.selectedLensFacing))
+                    }
                 }
             )
         }
